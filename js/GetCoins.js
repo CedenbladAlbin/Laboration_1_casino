867dbdb0caa1b6b183022f9a3f64e1a88e9c6731
AlbinCedenblad <albin.cedenblad@hotmail.com>
2025-09-22 10:38:53 +0200

    load currency in get coins
    load currency in get coins

--- js/GetCoins.js
+++ js/GetCoins.js
@@ -1,19 +1,31 @@
+
+import { addCurrency } from './Currency.js';
+
 let startcoins = 100;
+let counter = 0;
+
 
 function showSection(id) {
   const sections = ['sectionCoin', 'sectionWho', 'sectionHow', 'sectionSafehouse','sectionEarnings'];
   sections.forEach(sec => {
     document.getElementById(sec).style.display = 'none';
   });
- 
   document.getElementById(id).style.display = 'flex';
 }
+window.showSection = showSection;
 
 function updateCoinsEarned(multiplier){
-  startcoins = startcoins * multiplier
+  startcoins = startcoins * multiplier;
+  counter += 1;
   if(startcoins < 1){
     startcoins = 1;
   }
-  thediv = document.getElementById("coinsEarned");
-  thediv.innerText = "Earned coins: " + startcoins
+  const thediv = document.getElementById("coinsEarned");
+  thediv.innerText = "Earned coins: " + startcoins;
+  if(counter ===3){
+    console.log("Adding coins:", startcoins);
+    addCurrency(startcoins);
+    }
+  
 }
+window.updateCoinsEarned = updateCoinsEarned;
